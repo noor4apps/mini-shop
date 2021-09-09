b47413ddd3230e51c08939bb60ca560fe2065f87
noor4apps <noor4apps@gmail.com>
2021-09-09 14:24:59 +0300

    install and confige laravel passport and register&logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Http;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'type' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        if($validation->fails())
+        {
+            return response()->json(['errors' => true, 'messages' => $validation->errors()], 200);
+        }
+
+        $data['name'] = $request->name;
+        $data['type'] = $request->type;
+        $data['email'] = $request->email;
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+        // user attach role
+        return $this->getRefreshedToken($request->email, $request->password);
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return $this->getRefreshedToken($request->email, $request->password);
+        } else {
+            return response()->json(['errors' => true, 'messages' => 'Unauthorized'], 200);
+        }
+
+    }
+
+    public function getRefreshedToken($email, $password)
+    {
+        // if certificate https fake on localhost
+        $verifyValue = app()->environment() == 'local' ? false : true;
+
+        $response = Http::withOptions([
+            'verify' => $verifyValue,
+        ])->post(config('app.url') . '/oauth/token', [
+            'grant_type' => 'password',
+            'client_id' => config('passport.personal_access_client.id'),
+            'client_secret' => config('passport.personal_access_client.secret'),
+            'username' => $email,
+            'password' => $password,
+            'scope' => '*',
+        ]);
+
+        return response()->json($response->json(), 200);
+    }
+
+    public function refresh_token(Request $request)
+    {
+        try {
+            $refresh_token = $request->header('RefreshTokenCode');
+
+            // if certificate https fake on localhost
+            $verifyValue = app()->environment() == 'local' ? false : true;
+
+            $response = Http::withOptions([
+                'verify' => $verifyValue,
+            ])->post(config('app.url') . '/oauth/token', [
+                'grant_type' => 'refresh_token',
+                'refresh_token' => $refresh_token,
+                'client_id' => config('passport.personal_access_client.id'),
+                'client_secret' => config('passport.personal_access_client.secret'),
+                'scope' => '*',
+            ]);
+
+            return response()->json($response->json(), 200);
+        } catch (\Exception $e) {
+            return response()->json('Unauthorized', 200);
+        }
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,18 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Users\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register',            [AuthController::class, 'register']);
+Route::post('/login',               [AuthController::class, 'login']);
+Route::post('/refresh_token',       [AuthController::class, 'refresh_token']);
+
+Route::group(['middleware' => ['auth:api']], function () {
+
+    Route::get('/user_information',             [UsersController::class, 'user_information']);
+    Route::post('logout',                       [UsersController::class, 'logout']);
+
 });
